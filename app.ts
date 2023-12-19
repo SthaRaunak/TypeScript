e9fd8a36f0849658a599f41c36be6eaae6ebfc60
Raunak Shrestha <raunaknagendra@gmail.com>
2023-12-19 16:40:21 +0545

    typescript types vs javascript types

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 //working with core types using Type Script.
 
 function add(num1: number, num2: number) {
+  //Javascript approach:
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    throw new Error("Incorrect input types!");
+  }
+  //this fails at runtime instead this can easily be fixed in development with typescript
+  //but with typescript approach of defining data type we get error in development which is better experience for developers.
+  //as typescript is statically typed this allows us to define the types of variables and parameter and so on.. and so they dont change on runtime
+  //typescript only supports at development and not at runtime as all TS is converted into plain JS for brower to support
   return num1 + num2;
 }
 
