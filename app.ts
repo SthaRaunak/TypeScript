55a58ca81755a1d48af1643b501b1fb79805de02
Raunak Shrestha <raunaknagendra@gmail.com>
2023-12-19 17:40:23 +0545

    working with numbers,strings and booleans

--- app.ts
+++ app.ts
@@ -1,20 +1,15 @@
-//working with core types using Type Script.
-
-function add(num1: number, num2: number) {
-  //Javascript approach:
-  if (typeof num1 !== "number" || typeof num2 !== "number") {
-    throw new Error("Incorrect input types!");
+//working with Numbers , String ,Boolean
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
   }
-  //this fails at runtime instead this can easily be fixed in development with typescript
-  //but with typescript approach of defining data type we get error in development which is better experience for developers.
-  //as typescript is statically typed this allows us to define the types of variables and parameter and so on.. and so they dont change on runtime
-  //typescript only supports at development and not at runtime as all TS is converted into plain JS for brower to support
-  return num1 + num2;
 }
 
-const number1 = "5";
+const number1 = 5;
 const number2 = 2.8;
-
-const result = add(number1, number2);
-
-console.log(result);
+const printResult = true;
+const resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
