11b8baf312d6a339e6ed2e09ddfbfdaa49ff029d
Raunak Shrestha <raunaknagendra@gmail.com>
2023-12-21 21:10:20 +0545

    working with tuples

--- app.ts
+++ app.ts
@@ -1,25 +1,23 @@
-//Arrays in typescript
-
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+//Tuples type in typescript
+//Tuples : Java Script doesn't have tuple type. (Added by TypeScript : Fixed - length array)
+//A tuple is a typed array with a pre-defined length and types for each index.
+//For creating tuple we have to explicitly define the types of value in the array with fixed length
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // this is concept of tuple fixed with defined types
+} = {
   name: "Raunak",
   age: 17,
   hobbies: ["Guitar", "Programming"],
+  role: [2, "author"], // tuple
 };
 
-let favouriteActivites: string[];
+person.role.push("admin"); //exception  typescript allows push in an tuple were weve explicity defined only two types
 
-favouriteActivites = "Sports"; //gives an error as were expecting array of string instead
-favouriteActivites = ["Sports", 1]; // again an error number wasnt being expected could be avoided with any type
-favouriteActivites = ["Sports"]; // no error
-console.log(person.name); 
+person.role = [0 , 'admin' , 'user'] // but this here will give error because were defining anopther string in the tuple with predefined [number , string] tuple unlike the push exception this will give error
 
-for (const hobby of person.hobbies) {
-  //here hobby is automatically infered as string by typesciprt and lets us do all the string methods on it as it knows person.hobbies is a array of strings
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()) !!Error: map doesnt exist on type string
-}
+person.role[1] = 10; //this will give an error as were expecting the type of string on index 1
 
+//So we use tuple when we have the need to have exaclty x amount of values in array and know their types in advanced to maake the program strict to prevent errors
